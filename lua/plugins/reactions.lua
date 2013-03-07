eee270bcd4d6de0bd3578c4f3a19a209955de0ac
John Pirie <jpirie23@gmail.com>
2013-03-07 23:55:57 +0000

    Updated reaction files

--- lua/plugins/reactions.lua
+++ lua/plugins/reactions.lua
@@ -2,14 +2,15 @@ function reaction(username, serverPart, userMessage, isPrivateMessage)
   local botname = getBotName():lower()
   userMessage = userMessage:lower()
   if (string.find(userMessage, "oh boy")) then
-    sendLuaMessageToSource(username, "http://derpface.com/wp-content/uploads/2012/11/Oh-Boy-Reaction-Gif.gif", isPrivateMessage)
+    sendLuaMessageToSource(username, "www.jpirie.com/files/oh-boy.gif", isPrivateMessage)
   end
 --  if (string.find(userMessage, "imgur")) then
 --    sendLuaMessageToSource(username, "http://emotibot.net/pix/3455.gif", isPrivateMessage)
---  end
-  if (string.find(userMessage, "baffling")) then
-    sendLuaMessageToSource(username, "http://www.jpirie.com/files/baffling.gif", isPrivateMessage)
+  --  end
+  if (string.find(userMessage, "wowzers")) then
+    sendLuaMessageToSource(username, "http://i.imgur.com/xDOfr.gif", isPrivateMessage)
   end
+
 end
 
 
